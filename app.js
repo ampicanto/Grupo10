bf904afdf6c80837db5a6f3ca10e7b47def1ca13
ampicanto <barrazacantoamparo@gmail.com>
2025-04-28 10:05:49 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+let nombre = "Amparo"
+let age = 19
+let estudiante = true
+let comidafav = "ensalada"
+let pais = "Argentina"
+
+console.log("Hola, mi nombre es " + nombre + ", tengo " + age + " años y soy estudiante: " + estudiante + ". Mi comida favorita es " + comidafav + ".")
